f70e67fe80e6e6480bc1dcb4099dfe4db0315b9e
Jongwook98 <oucas1541@naver.com>
2024-09-13 12:33:38 +0900

    "TEST"

--- SSD/main.cpp
+++ SSD/main.cpp
@@ -6,7 +6,7 @@ int main() {
     std::cout << "hi2" << std::endl;
     std::cout << "hi2" << std::endl;
     std::cout << "hi2" << std::endl;
-
+    std::cout << "TEST " << std::endl;
 
 	return 0;
 }
